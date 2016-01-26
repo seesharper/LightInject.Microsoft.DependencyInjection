6417d1487e869e345662a231f09a2deeec275751
Bernhard Richter <bernhard.richter@gmail.com>
2016-01-26 15:27:26 +0100

    Initial commit

---
+++ src/LightInject.Microsoft.DependencyInjection.Tests/LightInjectSpecificationTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightInject.Microsoft.DependencyInjection.Tests
+{
+    using global::Microsoft.Extensions.DependencyInjection;
+    using global::Microsoft.Extensions.DependencyInjection.Specification;
+    public class Class1 : DependencyInjectionSpecificationTests
+    {
+        protected override IServiceProvider CreateServiceProvider(IServiceCollection serviceCollection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/LightInject.Microsoft.DependencyInjection/LightInject.Microsoft.DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightInject.Microsoft.DependencyInjection
+{
+    public class Class1
+    {
+    }
+}
