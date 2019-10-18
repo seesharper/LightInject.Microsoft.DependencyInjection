9dfc5f83fb229ae9117c0dcc855e53bea6d39093
Bernhard Richter <bernhard.richter@gmail.com>
2019-10-18 15:32:58 +0200

    Fixed version in source file

--- src/LightInject.Microsoft.DependencyInjection/LightInject.Microsoft.DependencyInjection.cs
+++ src/LightInject.Microsoft.DependencyInjection/LightInject.Microsoft.DependencyInjection.cs
@@ -21,7 +21,7 @@
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.
 ******************************************************************************
-    LightInject.Microsoft.DependencyInjection version 3.1.2
+    LightInject.Microsoft.DependencyInjection version 3.2.0
     http://www.lightinject.net/
     http://twitter.com/bernhardrichter
 ******************************************************************************/
