c82695eff8689659d9ef7cd293684de5e2db8930
Bernhard Richter <bernhard.richter@gmail.com>
2025-05-26 13:53:59 +0200

    Ensure that we populate service key type cache

--- src/LightInject.Microsoft.DependencyInjection.Tests/LightInjectKeyedSpecificationTests.cs
+++ src/LightInject.Microsoft.DependencyInjection.Tests/LightInjectKeyedSpecificationTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.CompilerServices;
 using System.Text;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Specification;
@@ -18,18 +19,18 @@ public class LightInjectKeyedSpecificationTests : KeyedDependencyInjectionSpecif
     public void ShouldHandleKeyedServiceWithEnumKey()
     {
         var serviceCollection = new ServiceCollection();
-        serviceCollection.AddKeyedTransient<IKeyedService, KeyedService>(Key.A);
+        serviceCollection.AddKeyedTransient<IKeyedServiceWithEnumServiceKey, KeyedServiceWithEnumServiceKey>(Key.A);
         var provider = CreateServiceProvider(serviceCollection);
-        provider.GetKeyedService<IKeyedService>(Key.A);
+        provider.GetKeyedService<IKeyedServiceWithEnumServiceKey>(Key.A);
     }
 
     [Fact]
     public void ShouldHandleKeyedServiceWithStringServiceKey()
     {
         var serviceCollection = new ServiceCollection();
-        serviceCollection.AddKeyedTransient<IKeyedService, KeyedServiceWithStringServiceKey>("A");
+        serviceCollection.AddKeyedTransient<IKeyedServiceWithStringServiceKey, KeyedServiceWithStringServiceKey>("A");
         var provider = CreateServiceProvider(serviceCollection);
-        var instance = provider.GetKeyedService<IKeyedService>("A");
+        var instance = provider.GetKeyedService<IKeyedServiceWithStringServiceKey>("A");
         Assert.Equal("A", ((KeyedServiceWithStringServiceKey)instance).ServiceKey);
     }
 
@@ -37,9 +38,9 @@ public class LightInjectKeyedSpecificationTests : KeyedDependencyInjectionSpecif
     public void ShouldHandleRequiredKeyedServiceWithStringServiceKeyUsingFactory()
     {
         var serviceCollection = new ServiceCollection();
-        serviceCollection.AddKeyedTransient<IKeyedService>(Key.A, (sp, key) => new KeyedServiceWithStringServiceKey((string)key));
+        serviceCollection.AddKeyedTransient<IKeyedServiceWithStringServiceKey>(Key.A, (sp, key) => new KeyedServiceWithStringServiceKey((string)key));
         var provider = CreateServiceProvider(serviceCollection);
-        var instance = provider.GetRequiredKeyedService<IKeyedService>("A");
+        var instance = provider.GetRequiredKeyedService<IKeyedServiceWithStringServiceKey>("A");
         Assert.Equal("A", ((KeyedServiceWithStringServiceKey)instance).ServiceKey);
     }
 
@@ -47,9 +48,9 @@ public class LightInjectKeyedSpecificationTests : KeyedDependencyInjectionSpecif
     public void ShouldHandleKeyedServiceWithEnumServiceKey()
     {
         var serviceCollection = new ServiceCollection();
-        serviceCollection.AddKeyedTransient<IKeyedService, KeyedServiceWithEnumServiceKey>(Key.A);
+        serviceCollection.AddKeyedTransient<IKeyedServiceWithEnumServiceKey, KeyedServiceWithEnumServiceKey>(Key.A);
         var provider = CreateServiceProvider(serviceCollection);
-        var instance = provider.GetKeyedService<IKeyedService>(Key.A);
+        var instance = provider.GetKeyedService<IKeyedServiceWithEnumServiceKey>(Key.A);
         Assert.Equal(Key.A, ((KeyedServiceWithEnumServiceKey)instance).ServiceKey);
     }
 
@@ -57,29 +58,29 @@ public class LightInjectKeyedSpecificationTests : KeyedDependencyInjectionSpecif
     public void ShouldHandleKeyedServiceWithIntServiceKey()
     {
         var serviceCollection = new ServiceCollection();
-        serviceCollection.AddKeyedTransient<IKeyedService, KeyServiceWithIntKey>(1);
+        serviceCollection.AddKeyedTransient<IKeyedServiceWithIntKey, KeyedServiceWithIntKey>(1);
         var provider = CreateServiceProvider(serviceCollection);
-        var instance = provider.GetKeyedService<IKeyedService>(1);
-        Assert.Equal(1, ((KeyServiceWithIntKey)instance).ServiceKey);
+        var instance = provider.GetKeyedService<IKeyedServiceWithIntKey>(1);
+        Assert.Equal(1, ((KeyedServiceWithIntKey)instance).ServiceKey);
     }
 
     [Fact]
     public void ShouldHandlePassingServiceKeyAsInteger()
     {
         var serviceCollection = new ServiceCollection();
-        serviceCollection.AddKeyedTransient<IKeyedService>(1, (sp, key) => new KeyServiceWithIntKey((int)key));
+        serviceCollection.AddKeyedTransient<IKeyedServiceWithIntKey>(1, (sp, key) => new KeyedServiceWithIntKey((int)key));
         var provider = CreateServiceProvider(serviceCollection);
-        var instance = provider.GetKeyedService<IKeyedService>(1);
-        Assert.Equal(1, ((KeyServiceWithIntKey)instance).ServiceKey);
+        var instance = provider.GetKeyedService<IKeyedServiceWithIntKey>(1);
+        Assert.Equal(1, ((KeyedServiceWithIntKey)instance).ServiceKey);
     }
 
     [Fact]
     public void ShouldHandlePassingServiceKeyAsEnum()
     {
         var serviceCollection = new ServiceCollection();
-        serviceCollection.AddKeyedTransient<IKeyedService>(Key.A, (sp, key) => new KeyedServiceWithEnumServiceKey((Key)key));
+        serviceCollection.AddKeyedTransient<IKeyedServiceWithEnumServiceKey>(Key.A, (sp, key) => new KeyedServiceWithEnumServiceKey((Key)key));
         var provider = CreateServiceProvider(serviceCollection);
-        var instance = provider.GetKeyedService<IKeyedService>(Key.A);
+        var instance = provider.GetKeyedService<IKeyedServiceWithEnumServiceKey>(Key.A);
         Assert.Equal(Key.A, ((KeyedServiceWithEnumServiceKey)instance).ServiceKey);
     }
 
@@ -87,18 +88,18 @@ public class LightInjectKeyedSpecificationTests : KeyedDependencyInjectionSpecif
     public void ShouldThrowExceptionWhenUsingInvalidKeyType()
     {
         var serviceCollection = new ServiceCollection();
-        serviceCollection.AddKeyedTransient<IKeyedService>(new StringBuilder(), (sp, key) => new KeyedServiceWithEnumServiceKey((Key)key));
+        serviceCollection.AddKeyedTransient<IKeyedServiceWithInvalidServiceKeyType>(new StringBuilder(), (sp, key) => new KeyedServiceWithInvalidServiceKeyType());
         var provider = CreateServiceProvider(serviceCollection);
-        Assert.Throws<InvalidOperationException>(() => provider.GetKeyedService<IKeyedService>(new StringBuilder()));
+        Assert.Throws<InvalidOperationException>(() => provider.GetKeyedService<IKeyedServiceWithInvalidServiceKeyType>(new StringBuilder()));
     }
 
     [Fact]
     public void ShouldHandleConvertibleType()
     {
         var serviceCollection = new ServiceCollection();
-        serviceCollection.AddKeyedTransient<IKeyedService>(1L, (sp, key) => new KeyedServiceWithLongServiceKey((long)key));
+        serviceCollection.AddKeyedTransient<IKeyedServiceWithLongServiceKey>(1L, (sp, key) => new KeyedServiceWithLongServiceKey((long)key));
         var provider = CreateServiceProvider(serviceCollection);
-        var instance = provider.GetKeyedService<IKeyedService>(1L);
+        var instance = provider.GetKeyedService<IKeyedServiceWithLongServiceKey>(1L);
         Assert.Equal(1L, ((KeyedServiceWithLongServiceKey)instance).ServiceKey);
     }
 
@@ -115,26 +116,38 @@ public class LightInjectKeyedSpecificationTests : KeyedDependencyInjectionSpecif
     public void ShouldInjectKeyedService()
     {
         var serviceCollection = new ServiceCollection();
-        serviceCollection.AddKeyedTransient<IKeyedService, KeyedService>("KeyedService");
-        serviceCollection.AddKeyedTransient<IKeyedService, AnotherKeyedService>("AnotherKeyedService");
-        serviceCollection.AddTransient<ServiceWithKeyedService>();
+        serviceCollection.AddKeyedScoped<IKeyedService>("KeyedService", (sp, key) => new KeyedService());
+        serviceCollection.AddKeyedScoped<IKeyedService>("AnotherKeyedService", (sp, key) => new AnotherKeyedService());
+        serviceCollection.AddScoped<IServiceWithKeyedService, ServiceWithKeyedService>();
         var provider = CreateServiceProvider(serviceCollection);
-        var instance = provider.GetService<ServiceWithKeyedService>();
+        using var scope = provider.CreateScope();
+        var instance = scope.ServiceProvider.GetRequiredService<IServiceWithKeyedService>();
         Assert.IsType<AnotherKeyedService>(instance.Service);
     }
 
     [Fact]
     public void ShouldInjectServiceFromDerivedServiceKey()
     {
-         var serviceCollection = new ServiceCollection();
+        var serviceCollection = new ServiceCollection();
         serviceCollection.AddKeyedTransient<IKeyedService, KeyedService>("KeyedService");
         serviceCollection.AddKeyedTransient<IKeyedService, AnotherKeyedService>("AnotherKeyedService");
         serviceCollection.AddTransient<ServiceWithDerivedServiceKey>();
         var provider = CreateServiceProvider(serviceCollection);
-        var instance = provider.GetService<ServiceWithDerivedServiceKey>();
+        var instance = provider.GetRequiredService<ServiceWithDerivedServiceKey>();
         Assert.IsType<AnotherKeyedService>(instance.Service);
     }
 }
+
+
+public interface IKeyedServiceWithInvalidServiceKeyType
+{
+}
+
+public class KeyedServiceWithInvalidServiceKeyType() : IKeyedServiceWithInvalidServiceKeyType
+{
+    
+}
+
 
 public interface IKeyedService
 {
@@ -145,24 +158,42 @@ public class KeyedService : IKeyedService
 
 }
 
-public class KeyedServiceWithStringServiceKey([ServiceKey] string serviceKey) : IKeyedService
+public interface IKeyedServiceWithStringServiceKey
+{
+    string ServiceKey { get; }
+}
+
+public class KeyedServiceWithStringServiceKey([ServiceKey] string serviceKey) : IKeyedServiceWithStringServiceKey
 {
     public string ServiceKey { get; } = serviceKey;
 }
 
-public class KeyedServiceWithEnumServiceKey([ServiceKey] Key serviceKey) : IKeyedService
+public interface IKeyedServiceWithEnumServiceKey
+{
+    Key ServiceKey { get; }
+}
+
+public class KeyedServiceWithEnumServiceKey([ServiceKey] Key serviceKey) : IKeyedServiceWithEnumServiceKey
 {
     public Key ServiceKey { get; } = serviceKey;
 }
 
+public interface IKeyedServiceWithIntKey
+{
+    int ServiceKey { get; }
+}
 
-public class KeyServiceWithIntKey([ServiceKey] int serviceKey) : IKeyedService
+public class KeyedServiceWithIntKey([ServiceKey] int serviceKey) : IKeyedServiceWithIntKey
 {
     public int ServiceKey { get; } = serviceKey;
 }
 
+public interface IKeyedServiceWithLongServiceKey
+{
+    long ServiceKey { get; }
+}
 
-public class KeyedServiceWithLongServiceKey([ServiceKey] long serviceKey) : IKeyedService
+public class KeyedServiceWithLongServiceKey([ServiceKey] long serviceKey) : IKeyedServiceWithLongServiceKey
 {
     public long ServiceKey { get; } = serviceKey;
 }
@@ -177,8 +208,12 @@ public class AnotherKeyedService : IKeyedService
 {
 }
 
+public interface IServiceWithKeyedService
+{
+    IKeyedService Service { get; }
+}
 
-public class ServiceWithKeyedService([FromKeyedServices("AnotherKeyedService")] IKeyedService service)
+public class ServiceWithKeyedService([FromKeyedServices("AnotherKeyedService")] IKeyedService service) : IServiceWithKeyedService
 {
     public IKeyedService Service { get; } = service;
 }
